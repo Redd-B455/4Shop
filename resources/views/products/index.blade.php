336d2078684f80620a5d52a108f6f5d6a43cce6c
Red Velvet <105586150+Redd-B455@users.noreply.github.com>
2023-09-26 18:01:45 +0200

    4Shop - Weekcheck II Completed

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -12,6 +12,9 @@
 						@unless(empty($product->description))
 							<p>{{ $product->description }}</p>
 						@endunless
+						@if($product->discount > 0)
+							<p style="color: red;">Nu <b>{{$product->discount}}%</b> korting!</p>
+						@endif
 					</div>
 					<button class="btn btn-primary">Meer info &amp; bestellen</button>
 				</div>
