b335bb429562b58c3b4020ac77392aed982048c5
Red Velvet <105586150+Redd-B455@users.noreply.github.com>
2023-09-26 15:35:16 +0200

    4Shop - Finished Weekcheck I
    Did everything as described in het Moduleboekje.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,11 +8,14 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Winkel - Scouting Raamsdonksveer</title>
+    <title>4Shop Dieder</title>
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/narrow-jumbotron.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
     <script src="/js/app.js"></script>
@@ -20,7 +23,10 @@
 <body>
     <div class="container">
       <header class="header d-flex justify-content-between align-items-center">
-        <h3 class="text-muted"><a href="{{ route('home') }}" class="no-link">Winkel van Scouting Raamsdonksveer</a></h3>
+        <div class="d-flex justify-content-between align-items-center">
+          <img class="imglogo" src="{{ url('img/4S-Logo-Compact-Small.png') }}" alt=""></img>
+          <h3 class="text-muted"><a href="{{ route('home') }}" class="no-link">4Shop</a></h3>
+        </div>
         <a href="{{ route('cart') }}"><img class="cart" src="{{ url('img/cart.png') }}" alt=""></a>
       </header>
 
@@ -44,7 +50,7 @@
       </main>
 
       <footer class="footer">
-        <p><strong>Mail bij vragen: <a href="mailto:winkelbeheer@scoutingrveer.nl">winkelbeheer@scoutingrveer.nl</a></strong> &middot; S.B.B.S. namens Scouting Raamsdonksveer, Kerklaan 22 4942AR Raamsdonksveer, KvK: 41100325, tel: 0162769096.</p>
+        <p>Een winkel van 4S. Terheijdense weg 450, Breda. Mail bij vragen naar <a href="mailto:info@4s.nl">info@4s.nl</a></p>
       </footer>
 
     </div>
